6528c136596047a1de6c6eca85b82279891d8675
Evelio Sosa <eveliososa@Evelios-MacBook-Pro.local>
2020-01-28 17:47:29 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".front-top-button").click(function(){
+        $(".front-nav").toggle();
+    });
+    $(".front-nav-button").click(function(){
+        $(".front-nav").toggle();
+    });
+});
